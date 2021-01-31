48082218698a29f298e2fca183f8de40cace0203
Lawrence-Ludwig <78214595+Lawrence-Ludwig@users.noreply.github.com>
2021-01-31 13:40:07 -0500

    Update Code.py

--- Code.py
+++ Code.py
@@ -26,8 +26,8 @@ def title_screen():
     if (title_input == 'C' or title_input == 'c' or title_input == 'Creddits' or title_input == 'creddits'):
         creddits()
     if (title_input == 'exit' or title_input == 'Exit' or title_input == 'e' or title_input == 'E'):
-    	sys.exit()
-    if (title_input == 'get' or title_input = 'Get'):
+        sys.exit()
+    if (title_input == 'get' or title_input == 'Get'):
         get_hub()
     restart()
     
@@ -46,21 +46,52 @@ def help_menu():
     print('Keep doing this intill the game stops asking for')
     print('Words.')
     print('Then it will show you your completed madlib')
-    print('')
-    print('')
+    print('Enjoy!')
+    input()
     restart()
 
 def creddits():
-    print('creddits working')
+    print('Link to code gethub:') 
+    print('https://github.com/Lawrence-Ludwig/Madlibs-Game')
     restart()
 
 
 def get_hub():
-    print('raw code working')
+    print('<Link>')
     restart()
 
 def start():
-    print('start working')
+    print('Type a verb')
+    verb1 = input('>>>')
+    print('Type a adjective')
+    adj1 = input('>>>')
+    print('Type a verb')
+    verb2 = input('>>>')
+    print('Type a part of body')
+    PoB1 = input('>>>')
+    print('Type a part of body')
+    PoB2 = input('>>>')
+    print('Type a noun')
+    noun1 = input('>>>')
+    print('Type a verb')
+    verb3 = input('>>>')
+    print('Type a animal')
+    animal1 = input('>>>')
+    print('Type a noun')
+    noun2 = input('>>>')
+    print('Type a verb')
+    verb4 = input('>>>')
+    print('Type a adjective')
+    adj2 = input('>>>')
+    print('Type a color')
+    color1 = input('>>>')
+    input('Are your reddy')
+    input('It doesnt mater because here it is')
+    print('Most doctors agree that bicycle ' + verb1 +' is a ' + adj1 + ' form of exercise. ' + verb2 + " a bicycle enables you to develob your " + PoB1 +  " mucles as well as increase the rate of your " + PoB2 + " beat. More " + noun1 + " around the world. " + verb3 + " bicycles than drive " + animal1 + ". No mater what kind of " + noun2 + " you " + verb4 + ", always be sure to wear a " + adj2 + " helmet. Make sure to have " + color1 + " reflectors too!" )
+
+
+
+    input()
     restart()
 
 title_screen()
